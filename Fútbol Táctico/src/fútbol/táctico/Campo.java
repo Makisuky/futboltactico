82be3ef4465750933163e44f9228aea12a8525b2
Luzy81 <luciamontoya503@gmail.com>
2024-10-22 01:34:42 -0500

    si

--- Fútbol Táctico/src/fútbol/táctico/Campo.java
+++ Fútbol Táctico/src/fútbol/táctico/Campo.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fútbol.táctico;
 
-/**
- *
- * @author alejo
- */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
--- Fútbol Táctico/src/fútbol/táctico/Jugador.java
+++ Fútbol Táctico/src/fútbol/táctico/Jugador.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fútbol.táctico;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- *
- * @author alejo
- */
+
 public class Jugador {
 
     String nombre;
